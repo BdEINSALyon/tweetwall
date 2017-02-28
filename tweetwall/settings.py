7307fa7cc0adea2d8c121f868be6b59bd104148a
Philippe Vienne <philippegeek@gmail.com>
2017-02-28 04:40:02 +0100

    Create moderation app

--- tweetwall/settings.py
+++ tweetwall/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.SocialConfig',
+    'moderation.apps.ModerationConfig',
     'account.apps.AccountConfig',
     'permissions.apps.PermissionsConfig',
     'graphene_django'
